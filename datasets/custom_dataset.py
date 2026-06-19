0d8661eb76cd740c297f2679d1629a123df9485c
Mustafa-SW <mustafasw99@gmail.com>
2026-06-19 16:53:00 +0530

    Learn about custom datasets and data loader

--- datasets/custom_dataset.py
+++ datasets/custom_dataset.py
@@ -2,13 +2,26 @@ import pandas as pd
 import torch
 from torch.utils.data import Dataset
 
+# Dataset is an abstract class in pytorch which allows it to load 
+# and process samples one by one
+
 class ActionDataset(Dataset):
 
     def __init__(self, csv_file):
-        self.data = pd.read_csv(csv_file)
+        self.data = pd.read_csv(csv_file) 
+    
+    # pandas.read_csv returns pandas.data_frame
+    # A DataFrame is a two-dimensional, size-mutable, and potentially heterogeneous
+    # tabular data structure with labeled axes (rows and columns)
+
 
     def __len__(self):
         return len(self.data)
+    
+    # the __getitem__ method allows you to treat the dataset object as a Python list
+    # allowing you to access its rows by using square bracket indexing 
+    # here particularly this method returns features and labels of the row 
+    # whose index is provided as pytorch tensors
 
     def __getitem__(self, idx):
 
--- train.py
+++ train.py
@@ -6,6 +6,9 @@ dataset = ActionDataset("data/sample_data.csv")
 
 print("Dataset Size:", len(dataset))
 
+# DataLoader is a built-in PyTorch utility. Its job is to handle batching, shuffling, 
+# and managing CPU memory threads when feeding data to an AI model
+
 loader = DataLoader(
     dataset,
     batch_size=2,
@@ -21,3 +24,8 @@ for features, labels in loader:
     print(labels)
 
     break
+
+# Why use break here?  Instead of letting the loop run forever 
+# and printing thousands of rows to your console, 
+# adding break allows you to inspect just one single batch to make sure 
+# the shapes, dimensions, and data types match exactly
